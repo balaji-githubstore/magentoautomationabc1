4a35c414c6026c7da3ae261d90a83266c14fbe29
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2018-06-15 13:03:35 +0530

    Second commit

--- src/test/java/com/abc/magentotest/MagentoTest.java
+++ src/test/java/com/abc/magentotest/MagentoTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -16,7 +17,7 @@ public class MagentoTest {
 	@Test
 	public void magentoTest() {
 		// TODO Auto-generated method stub
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://magento.com/");
